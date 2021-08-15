bec52c1807d90bc6531133057482ed0b8e909093
kelvin beno <kelvinbeno526@gmail.com>
2021-08-15 23:19:21 +0300

    home page updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Home from './components/Home/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,7 +12,7 @@ function App() {
      <NavBar/>
    </div>
    <div>
-     Cards
+     <Home />
    </div>
     </div>
   );
